4b123a00b6efeb4fecc8fc84ed9f3c625c509b92
Leonard.Cristescu <60926@gymnasium.ax>
2024-10-11 10:51:43 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { provide, ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+const HomeViews = ref(0);
+provide('HomeViews', HomeViews);
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { inject, onMounted, ref } from 'vue';
+const views = inject('HomeViews')
+onMounted(() => {
+  views.value++
+})
+const tid = ref(new Date().toLocaleString())
+</script>
+
+<template>
+  <main>
+    <p>
+      När sidan laddades var klockan...<br>
+      {{ tid }}
+    </p>
+
+    <p>Denna sidan har visats:
+      <b>{{ views }}</b> gånger
+    </p>
+  </main>
+</template>
